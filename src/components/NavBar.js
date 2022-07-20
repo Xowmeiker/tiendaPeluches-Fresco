a4adc5827bbd9e3c32e65dad44ba9cce82c3d118
Xowmeiker <fresco.nicolas.work@gmail.com>
2022-07-20 11:54:06 +1000

    creado componente para barra navegacion

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+function NavBar() {
+    return (
+        <header>
+            <h1>Stuffed</h1>
+            <nav>
+                <li>
+                    <a href="#">Animales</a>
+                    <a href="#">Personajes</a>
+                    <a href="#">Almohadas</a>
+                </li>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
