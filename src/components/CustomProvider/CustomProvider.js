c9abd2186fe26c03035061d852f6d5ff9dd4e47e
Xowmeiker <fresco.nicolas.work@gmail.com>
2022-08-31 10:32:37 +1000

    codigo refactorizado para tener mas claridad, instaladas las dependencias de firebase, falta implementar

--- src/components/CustomProvider/CustomProvider.js
+++ src/components/CustomProvider/CustomProvider.js
@@ -1,56 +1,66 @@
-import React from 'react'
-import { useState } from 'react';
-import { createContext } from 'react'
+import React from "react";
+import { useState } from "react";
+import { createContext } from "react";
 
 export const context = createContext({});
-const {Provider} = context;
+const { Provider } = context;
 
 export default function CustomProvider(props) {
-    const [product,setProduct] = useState({});
+  const [product, setProduct] = useState({});
 
-    const addProduct = (product)=>{
-
-        let productInCartIndex = contextValue.cart.map(e=>e.id).indexOf(product.id);
-
-        if(productInCartIndex !== -1) {
-            contextValue.cart[productInCartIndex].quantity++
-        }else{
-            contextValue.cart.push(product)
-        }
+  const addProduct = (product) => {
+    if (isInCart(product.id)) {
+      changeQuantityOf(product.id, 1);
+    } else {
+      contextValue.cart = [...contextValue.cart, product];
     }
+  };
 
-    const deleteProduct = (product)=>{
-
-        let productInCartIndex = contextValue.cart.map(e=>e.id).indexOf(product.id);
-
-        if(productInCartIndex !== -1 && contextValue.cart[productInCartIndex].quantity > 1) {
-            contextValue.cart[productInCartIndex].quantity--
-        }else{
-            contextValue.cart.splice(productInCartIndex,1)
-        }
+  const deleteProduct = (product) => {
+    if (isInCart(product.id) && quantityInCartOf(product.id) > 1) {
+      changeQuantityOf(product.id, 1);
+    } else {
+        remove(contextValue.cart,indexInCartOf(product.id));
     }
+  };
 
-    const emptyCart = ()=>{
-        contextValue.cart = []
-    }
+  const emptyCart = () => {
+    contextValue.cart = [];
+  };
 
-    const isInCart = (id)=>{
-        return contextValue.cart.map(e=>e.id).indexOf(id) !== -1? true:false;
-    }
+  const isInCart = (id) => {
+    return contextValue.cart.some((e) => e.id == id);
+  };
 
-    const contextValue = {
-        cart:[],
-        addProduct,
-        deleteProduct,
-        emptyCart,
-        isInCart,
-        Product:[product,
-            setProduct]
-    }
+  const quantityInCartOf = (id) => {
+    let productInCartIndex = contextValue.cart.map((e) => e.id).indexOf(id);
+    return contextValue.cart[productInCartIndex].quantity;
+  };
 
-  return (
-    <Provider value={contextValue}>
-        {props.children}
-    </Provider>
-  )
+  const indexInCartOf = (id) => {
+    return contextValue.cart.map((e) => e.id).indexOf(id);
+  };
+
+  const changeQuantityOf = (id, quantity) => {
+    let productIndex = indexInCartOf(id);
+    contextValue.cart[productIndex].quantity += quantity;
+  };
+
+  const remove = (cart, index) => {
+    return [
+      ...cart.slice(0, index),
+      ...cart.slice(index + 1),
+    ];
+  };
+
+  const contextValue = {
+    cart: [],
+    addProduct,
+    deleteProduct,
+    emptyCart,
+    isInCart,
+    Product: [product, setProduct],
+  };
+
+  return <Provider value={contextValue}>{props.children}</Provider>;
 }
---
+++ src/components/FirebaseProvider/FirebaseProvider.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCofEeMkfWyGFySKk9VGXMAQGlRDN0-_NU",
+  authDomain: "stuffed-01.firebaseapp.com",
+  projectId: "stuffed-01",
+  storageBucket: "stuffed-01.appspot.com",
+  messagingSenderId: "672021841020",
+  appId: "1:672021841020:web:722117fd01121cfaf335f9",
+  measurementId: "G-J1D3H917RX",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+
+export default function FirebaseProvider(props) {
+  return <>
+  {props.children}
+ </>;
+}
